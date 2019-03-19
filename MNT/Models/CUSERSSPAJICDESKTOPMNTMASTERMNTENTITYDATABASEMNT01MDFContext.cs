05f475460addaa999cc5a9fd9338caa9f1eb190c
pmarija93 <marijapavkovic21@gmail.com>
2019-03-19 13:20:28 +0100

    update conection string

--- MNT/Models/CUSERSSPAJICDESKTOPMNTMASTERMNTENTITYDATABASEMNT01MDFContext.cs
+++ MNT/Models/CUSERSSPAJICDESKTOPMNTMASTERMNTENTITYDATABASEMNT01MDFContext.cs
@@ -32,8 +32,7 @@ namespace MNT.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\spajic\\Desktop\\MNT-master\\MNT.Entity\\Database\\MNT01.mdf;Integrated Security=True;");
+                optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\User\\Desktop\\MNT-master\\MNT.Entity\\Database\\MNT01.mdf;Integrated Security=True;");
             }
         }
 
